1e0cad322fb6d563b0656d42b69912268b0f3aa3
Yun <wyyalt@live.com>
2017-10-10 23:42:48 +0800

    website

--- PigeonEye/settings.py
+++ PigeonEye/settings.py
@@ -120,5 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'statics'),
+)
+
 
 SESSION_TRACKER_SCRIPT = os.path.join(BASE_DIR,'Audit/backend/session_tracker.sh')
